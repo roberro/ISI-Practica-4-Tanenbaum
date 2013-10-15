5c8e5fc31145b96704d86ffd9c52c9b22bedff5f
Alejandro G-G Pérez <alex.ggperez@gmail.com>
2013-10-15 19:29:07 +0200

    corregido el collide

--- 10-refactor/spec/GameBoardSpec.js
+++ 10-refactor/spec/GameBoardSpec.js
@@ -40,3 +40,108 @@ Especificación: GameBoard debe
 
 
 */
+describe("Clase GameBoard", function(){
+   
+    beforeEach(function(){
+        loadFixtures('index.html');
+
+        canvas = $('#game')[0];
+        expect(canvas).toExist();
+
+        ctx = canvas.getContext('2d');
+        expect(ctx).toBeDefined();
+    });
+
+    // add()
+    it("add()", function(){
+        var miGameBoard = new GameBoard();
+        var obj = "nave";
+        miGameBoard.add(obj);
+        expect(miGameBoard.add(obj)).toEqual("nave");
+    });
+
+        // remove()
+    it("remove(), resetRemoved, finalizeRemoved", function(){
+        var miGameBoard = new GameBoard();
+        var obj1 = "nave1";
+        var obj2 = "nave2";
+        var obj3 = "nave3";
+        miGameBoard.add(obj1);
+        miGameBoard.add(obj2);
+        miGameBoard.add(obj3);
+
+        miGameBoard.resetRemoved();
+        miGameBoard.remove(obj2);
+        miGameBoard.finalizeRemoved();
+
+        expect(miGameBoard.objects[0]).toEqual("nave1");
+        expect(miGameBoard.objects[1]).toEqual("nave3");
+        expect(miGameBoard.objects[2]).toEqual(undefined);
+    });
+
+
+
+   it("interacción con Game", function(){
+ 
+        //Creamos dos objetos en la lista
+        var board = new GameBoard();
+        board.add(new PlayerShip());
+        board.add(new PlayerShip());
+        expect(board.objects.length).toEqual(2);
+        
+        //Programamos los spys para cada función de cada objeto
+        spyOn(board.objects[1], "step");
+        spyOn(board.objects[0], "step");
+        spyOn(board.objects[1], "draw");
+        spyOn(board.objects[0], "draw");
+        
+        //Comprobamos que al llamar la función en GameBoard, ésta se ocupa
+        //de ejecutar los metodos en cada uno de los objetos de la lista
+        var dt = 1;
+        board.step(dt);
+        expect(board.objects[0].step).toHaveBeenCalled();
+        expect(board.objects[1].step).toHaveBeenCalled();
+ 
+        board.draw(ctx);
+        expect(board.objects[1].draw).toHaveBeenCalled();
+        expect(board.objects[0].draw).toHaveBeenCalled();
+
+    }); 
+
+
+    it("detect", function(){
+
+			var miNave = new GameBoard();
+			var obj = "nave";
+	
+			miNave.add(obj);
+			
+			var func = {call: function(){}};
+			spyOn(func, "call");
+			miNave.detect(func)
+			waits(100);
+		
+			expect(miNave.objects[0]).toBeTruthy();
+			expect(miNave.objects[1]).toBeFalsy();
+
+    });
+
+    it("overlap", function(){
+		var miNave = new GameBoard();
+		var o1 = {x:2,y:2,h:1,w:1};
+		var o2 = {x:1,y:1,h:2,w:2};
+
+		miNave.add(o1);
+		miNave.add(o2);
+
+		expect(miNave.overlap(o1,o2)).toBeTruthy();	
+    });
+
+    it("collide", function(){
+		var miNave = new GameBoard();
+		miNave.objects = [{type:"1",x:2,y:2,h:1,w:1},{type:"2",x:1,y:1,h:2,w:2}];
+		var obj = {x:2,y:2,h:1,w:1};
+
+		expect(miNave.collide(obj,"1")).toEqual(miNave.objects[0]);
+    });
+ });
